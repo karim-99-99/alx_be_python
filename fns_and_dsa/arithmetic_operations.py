21231c3c6892f26529dbcc1693d5d0ff95bb979e
karim-99-99 <kareem.hossam99@yahoo.com>
2025-07-25 11:53:42 +0300

    new day new lesson

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -5,10 +5,10 @@ def perform_operation(num1, num2, operation):
         return num1 - num2
     elif operation == 'multiply':
         return num1 * num2
-    elif num2 == 0 and operation == 'divide':
-        return "Division by zero error"
     elif operation == 'divide':
-        return num1 / num2
+        if num2 == 0:
+            return "Division by zero error"
+        else:
+            return num1 / num2
     else:
         return "Invalid operation"
-    
