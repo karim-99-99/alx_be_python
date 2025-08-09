e6e619bc947304766b216d811fe6f49ecbcbce07
karim-99-99 <kareem.hossam99@yahoo.com>
2025-08-09 22:48:31 +0300

    new day new lesson

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -9,7 +9,7 @@ class Circle(Shape):
         self.radius = radius
 
     def area(self):
-        return math.pi * self.radius * self.radius
+        return math.pi * (self.radius ** 2)
 
 class Rectangle(Shape):
     def __init__(self, width, height):
